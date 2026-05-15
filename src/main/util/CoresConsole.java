688af88e8364fe21dd33b0817cb25880fd3c7356
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-05-15 15:17:15 -0300

    feat: adiciona utilitário de cores do console

---
+++ src/main/util/CoresConsole.java
@@ -0,0 +1,19 @@
+package util;
+
+public class CoresConsole {
+
+    public static final String RESET =
+            "\033[0m";
+
+    public static final String VERDE =
+            "\033[0;32m";
+
+    public static final String VERMELHO =
+            "\033[0;31m";
+
+    public static final String CIANO =
+            "\033[0;36m";
+
+    public static final String AMARELO =
+            "\033[0;33m";
+}
